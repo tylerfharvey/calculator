f29c8faae1156c40ab43fd0d3f534fe354442ab1
tylerfharvey <83512356+tylerfharvey@users.noreply.github.com>
2021-12-24 01:01:06 -0500

    Commit

--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-/*import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();*/
-
-
 
 const calcItems =[
   { id: "clear", text: 'CLR', class: 'calc-button bkg3', style: {gridColumnStart: "span 3"} },
@@ -39,6 +20,9 @@ const calcItems =[
 ]
 
 class CButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div 
